be8bfb8672686bb4a256005d333d19d7a023e083
Lerato <42895172+Leratoyoungling@users.noreply.github.com>
2018-09-17 21:27:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-
+var userChoices = document.getElementsByTagName('input[type:radio]');
+      var questions = 
+      [
+        {
+          question: "What is the capital of United Kingdom?",
+          choices: ["Manchester", "Birmingham", "London", "Birmingham"],
+          answer: 2
+        },
+        
+        {
+          question: "What is the capital of United States?",
+          choices: ["California", "New York", "Miami", "Florida"],
+          answer: 1
+        }
+        
+        
+      ];
+      
+      for ( var i = 0; i < questions.length; i++ ) {
+        var question = questions[i].question;
+        document.write ( question );
+        var options = questions[i].choices;
+        for ( var opt in options ) {
+           for ( var radios in userChoices ) {
+             userChoices[radios].value = options[opt];
+             
+           }
+        }
+        
+      }
