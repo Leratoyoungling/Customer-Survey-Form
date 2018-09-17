5a92b040ff20131d28be761416c3dcf9bc8cfc6b
Lerato <42895172+Leratoyoungling@users.noreply.github.com>
2018-09-17 22:01:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var questions = [
       {
-           prompt: "What color are apples?\n(a) Red/Green\n\ (b) Purple\n(c) Orange",
+           prompt: "What color are apples?\n(a) Red/Green\n (b) Purple\n(c) Orange",
            answer: "a"
      },
      {
-          prompt: "What color are Bananas?\n(a) Teal\n\ (b) Magenta\n(c) Yellow",
+          prompt: "What color are Bananas?\n(a) Teal\n (b) Magenta\n(c) Yellow",
           answer: "c"
      },
      {
-          prompt: "What color are strawberries?\n(a) Yellow\n\ (b) Red\n(c) Blue",
+          prompt: "What color are strawberries?\n(a) Yellow\n (b) Red\n(c) Blue",
           answer: "b"
      }
      ];
