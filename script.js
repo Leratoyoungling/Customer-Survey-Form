88dac148dfb60976655a870cb3197694c1d0a0d7
Lerato <42895172+Leratoyoungling@users.noreply.github.com>
2018-09-20 15:37:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
+var questions = [
+      {
+           prompt: "What color are apples?\n(1) Red/Green\n (2) Purple\n(3) Orange",
+           answer: "1"
+     },
+     {
+          prompt: "What color are Bananas?\n(1) lime\n (2) Magenta\n(3) Yellow",
+          answer: "3"
+     },
+     {
+          prompt: "What color are strawberries?\n(1) Yellow\n (2) Red\n(3) Blue",
+          answer: "2"
+     }
+     ];
 
+var score = 0;
+for(var i = 0; i < questions.length; i++){
+     var response = document.prompt(questions[i].prompt);
+     if(response == questions[i].answer){
+          score++;
+         document.write("Correct!");
+     } else {
+          document.write("WRONG!");
+     }
+}
+document.write("you got " + score + "/" + questions.length);
 
