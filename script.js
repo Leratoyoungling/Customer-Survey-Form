f238143548fc3ee96bd8b0145705ead8f2485eea
Lerato <42895172+Leratoyoungling@users.noreply.github.com>
2018-09-20 16:04:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ for(var i = 0; i < questions.length; i++){
      var response = window.prompt(questions[i].prompt);
      if(response == questions[i].answer){
           score++;
-         window.write("Correct!");
+         alert("Correct!");
      } else {
-          window.write("WRONG!");
+          alert("WRONG!");
      }
 }
-window.write("you got " + score + "/" + questions.length);
+alert("you got " + score + "/" + questions.length);
 
