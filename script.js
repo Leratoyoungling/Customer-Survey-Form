96438679923dc5567b09e39cf4ae123cb31f7885
Lerato <42895172+Leratoyoungling@users.noreply.github.com>
2018-09-24 20:37:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 Submit = function(){
-    alert("Information Submitted");
+    var Name = document.getElementById('name').value;
+    var Email = document.getElementById('Email').value;
+    
+     var number-label = document.getElementById('number-label').value;
+     
+    }
+    
+    document.write("<h1>Thank You!<h1/>");
+    document.write("<p>Now, your data will send via email.<p/>");
+    
+    document.write("<h3>Here is your data:</h3>");
+    
+    document.write(Name + "<br/>");
+    document.write(Email + "<br/>");
+                                      
 }
 
