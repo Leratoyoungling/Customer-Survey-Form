bddbcd8e0700b6babedd316c516972958a710416
Lerato <42895172+Leratoyoungling@users.noreply.github.com>
2018-09-27 14:22:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-Submit = function(){
-    alert("Survey Submitted!");
-    alert("Thank You for your participation");
-}
+        Submit = function()
+        {
+          var x;
+          x = document.getElementById("input-field").value;
+          if (x == "") 
+           { 
+              alert("Enter a required fields");
+           };
+	  else {
+          alert("Survey Submitted!");
+    	  alert("Thank You for your participation");
+		}
+        }
 
 
