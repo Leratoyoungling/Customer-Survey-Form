8562e5b868f8929d3632846d8b8154dc304a2bd4
Lerato <42895172+Leratoyoungling@users.noreply.github.com>
2018-09-27 13:55:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 Submit = function(){
     alert("Survey Submitted!");
     alert("Thank You for your participation");
+    
+    var isValid = true;
+     $('input,textarea,select').filter('[required]:visible').each(function() {
+      if ( $(this).val() === '' )
+         isValid = false;
+    });
+    if( isValid ) {
+       $('#submitBtn').prop('disabled', false);
+    } else {
+       $('#submitBtn').prop('disabled', true);
+    };
+    $('#submitBtn').click(function() {
+        var isValid = true;
+         $('input,textarea,select').filter('[required]:visible').each(function() {
+          if ( $(this).val() === '' )
+             isValid = false;
+        });
+        if( isValid ) {
+           $("#myForm")[0].submit(); 
+       };
+
+    });
 }
+
 
